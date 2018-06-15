72c5f6cdef8d783d366ad171417e26770872c365
Oleksandr Danylchuk <imdasash@gmail.com>
2018-06-14 21:09:04 -0400

    added maven project

---
+++ src/main/java/com/calculator/App.java
@@ -0,0 +1,25 @@
+package com.calculator;
+
+/**
+ * Calculator app
+ *
+ */
+public class App {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int subtraction(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplication(int a, int b) {
+		return a * b;
+	}
+
+	public int divison(int a, int b) {
+		return a / b;
+	}
+
+}
---
+++ src/test/java/com/calculator/AppTest.java
@@ -0,0 +1,31 @@
+package com.calculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	
+	public static void InitCalculator() {
+		
+		App cal = new App();
+		
+	}
+    
+	
+    @Test
+    public void testSum(){
+    //	Assert.assertEquals(2,cal.sum(1,1));   // ?it doesnt work
+    	
+        
+    }
+}
